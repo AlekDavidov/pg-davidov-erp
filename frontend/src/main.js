f41b66b9c30547f82ea350afccf85a08847f6160
Aleksandar Davidov <aleksandardavidov91@gmail.com>
2026-08-01 15:59:46 +0200

    feat: add invoice document management and application branding

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import ConfirmationService from 'primevue/confirmationservice'
 import ToastService from 'primevue/toastservice'
+
+import { definePreset } from '@primeuix/themes'
 import Aura from '@primeuix/themes/aura'
 
 import 'primeicons/primeicons.css'
@@ -12,12 +14,57 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
+const PgDavidovPreset = definePreset(Aura, {
+    primitive: {
+        davidovBlue: {
+            50: '#eef6ff',
+            100: '#d9eaff',
+            200: '#bddcff',
+            300: '#90c6ff',
+            400: '#5ca7f5',
+            500: '#2d73c8',
+            600: '#245fa8',
+            700: '#204f87',
+            800: '#1e446f',
+            900: '#1d3a5d',
+            950: '#13263e'
+        }
+    },
+
+    semantic: {
+        primary: {
+            50: '{davidovBlue.50}',
+            100: '{davidovBlue.100}',
+            200: '{davidovBlue.200}',
+            300: '{davidovBlue.300}',
+            400: '{davidovBlue.400}',
+            500: '{davidovBlue.500}',
+            600: '{davidovBlue.600}',
+            700: '{davidovBlue.700}',
+            800: '{davidovBlue.800}',
+            900: '{davidovBlue.900}',
+            950: '{davidovBlue.950}'
+        },
+
+        focusRing: {
+            width: '2px',
+            style: 'solid',
+            color: '{primary.400}',
+            offset: '2px'
+        }
+    }
+})
+
 createApp(App)
     .use(createPinia())
     .use(router)
     .use(PrimeVue, {
         theme: {
-            preset: Aura
+            preset: PgDavidovPreset,
+            options: {
+                darkModeSelector: '.app-dark',
+                cssLayer: false
+            }
         }
     })
     .use(ConfirmationService)
